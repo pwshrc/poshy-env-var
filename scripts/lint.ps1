d32c414c37c7932c66ebe69c2bf3eca863ad4be4
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-20 16:44:17 -0400

    👷 Updates Linting Script

--- scripts/lint.ps1
+++ scripts/lint.ps1
@@ -1,4 +1,7 @@
 #!/usr/bin/env pwsh
+param(
+    [switch] $NoFail
+)
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
@@ -6,11 +9,11 @@ Set-StrictMode -Version Latest
 if (-not (Get-Command Invoke-ScriptAnalyzer -ErrorAction SilentlyContinue)) {
     throw "Invoke-ScriptAnalyzer not found. Please install the PowerShell module 'PSScriptAnalyzer'."
 }
-
-Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\*.ps1","$PSScriptRoot\..\*.psm1" -Recurse -ReportSummary -OutVariable issues
+$ds = [System.IO.Path]::DirectorySeparatorChar
+Invoke-ScriptAnalyzer -Path "${PSScriptRoot}${ds}..${ds}**${ds}*" -Recurse -ReportSummary -OutVariable issues
 $errors   = $issues.Where({$_.Severity -eq 'Error'})
 $warnings = $issues.Where({$_.Severity -eq 'Warning'})
-if ($errors) {
+if ($errors -and (-not $NoFail)) {
     Write-Error "There were $($errors.Count) errors and $($warnings.Count) warnings total." -ErrorAction Stop
 } else {
     Write-Output "There were $($errors.Count) errors and $($warnings.Count) warnings total."
