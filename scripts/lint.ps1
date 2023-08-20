dfe46f976ae09d5ffd164423f1712bb59b49c3de
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-20 13:57:55 -0400

    👷 Adds Linting Script

---
+++ scripts/lint.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+
+if (-not (Get-Command Invoke-ScriptAnalyzer -ErrorAction SilentlyContinue)) {
+    throw "Invoke-ScriptAnalyzer not found. Please install the PowerShell module 'PSScriptAnalyzer'."
+}
+
+Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\*.ps1" -Recurse -ReportSummary -OutVariable issues
+$errors   = $issues.Where({$_.Severity -eq 'Error'})
+$warnings = $issues.Where({$_.Severity -eq 'Warning'})
+if ($errors) {
+    Write-Error "There were $($errors.Count) errors and $($warnings.Count) warnings total." -ErrorAction Stop
+} else {
+    Write-Output "There were $($errors.Count) errors and $($warnings.Count) warnings total."
+}
