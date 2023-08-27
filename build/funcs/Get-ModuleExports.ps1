fd03189322c984f37e75274b51330019d64c40ab
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-27 16:52:23 -0400

    👷 Updates Build Scripts

--- build/funcs/Get-ModuleExports.ps1
+++ build/funcs/Get-ModuleExports.ps1
@@ -19,32 +19,45 @@ function Get-ModuleExports {
         "DscResources" = @()
         "FormatFiles" = @()
     }
-    [bool] $madeGlobalVariable = $false
-    if (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ErrorAction SilentlyContinue)) {
-        $madeGlobalVariable = $true
-        Set-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Value $true -Scope Global -Option ReadOnly -Force
-    }
+    [string] $ds = [System.IO.Path]::DirectorySeparatorChar
+    . "${PSScriptRoot}${ds}Import-PSGalleryModuleNested.ps1"
+    [PSModuleInfo[]] $modulesToUnload = $null
+    $modulesToUnload = Import-PSGalleryModuleNested -RuntimeDependencies -SkipAlreadyLoaded
     try {
+        [bool] $madeGlobalVariable = $false
+        if (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ErrorAction SilentlyContinue)) {
+            $madeGlobalVariable = $true
+            Set-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Value $true -Scope Global -Option ReadOnly -Force
+        }
         try {
-            [System.Management.Automation.PSModuleInfo] $moduleInfo = Import-Module -Name $Psm1Path -Force -DisableNameChecking -PassThru
-            if ($null -eq $moduleInfo) {
-                throw "Failed to import module from path '$Psm1Path'."
+            [System.Management.Automation.PSModuleInfo] $moduleInfo = $null
+            $moduleInfo = Import-Module -Name $Psm1Path -NoClobber -Force -DisableNameChecking -PassThru
+            try {
+                if ($null -eq $moduleInfo) {
+                    throw "Failed to import module from path '$Psm1Path'."
+                }
+                $results["Functions"] += @($moduleInfo.ExportedFunctions.Keys)
+                $results["Cmdlets"] += @($moduleInfo.ExportedCmdlets.Keys)
+                $results["Commands"] += @($moduleInfo.ExportedCommands.Keys)
+                $results["Aliases"] += @($moduleInfo.ExportedAliases.Keys)
+                $results["Variables"] += @($moduleInfo.ExportedVariables.Keys)
+                $results["DscResources"] += @($moduleInfo.ExportedDscResources)
+                $results["FormatFiles"] += @($moduleInfo.ExportedFormatFiles)
+            } finally {
+                if ($null -ne $moduleInfo) {
+                    $moduleInfo | Remove-Module -Force
+                }
             }
-            $results["Functions"] += @($moduleInfo.ExportedFunctions.Keys)
-            $results["Cmdlets"] += @($moduleInfo.ExportedCmdlets.Keys)
-            $results["Commands"] += @($moduleInfo.ExportedCommands.Keys)
-            $results["Aliases"] += @($moduleInfo.ExportedAliases.Keys)
-            $results["Variables"] += @($moduleInfo.ExportedVariables.Keys)
-            $results["DscResources"] += @($moduleInfo.ExportedDscResources)
-            $results["FormatFiles"] += @($moduleInfo.ExportedFormatFiles)
         } finally {
-            if ($null -ne $moduleInfo) {
-                $moduleInfo | Remove-Module -Force
+            if ($madeGlobalVariable) {
+                Remove-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -Force
             }
         }
     } finally {
-        if ($madeGlobalVariable) {
-            Remove-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -Force
+        if ($null -ne $modulesToUnload) {
+            [array]::Reverse($modulesToUnload) `
+                | Select-Object -ExpandProperty path -Unique `
+                | Remove-Module -Force -ErrorAction SilentlyContinue
         }
     }
 
