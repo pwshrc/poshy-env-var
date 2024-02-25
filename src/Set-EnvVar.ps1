ff97050e789f7412c6aa5a16784b5b2f574f0cc3
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-02-25 17:41:37 -0500

    ✨ Makes Process-scope Default for `Set-EnvVar`

--- src/Set-EnvVar.ps1
+++ src/Set-EnvVar.ps1
@@ -5,75 +5,76 @@ Set-StrictMode -Version Latest
 
 <#
 .SYNOPSIS
-    Sets the value of the specified environment variable at the given environment variable scope.
+    Sets the value of the specified environment variable, optionally at the given environment variable scope.
 .PARAMETER Process
     Sets the value of the environment variable at the Process-level environment variable scope.
 .PARAMETER User
     Sets the value of the environment variable at the User-level environment variable scope.
-    If running on Windows and the current session is not elevated, causes an exception to be thrown.
+    Not supported on non-Windows platforms.
 .PARAMETER Machine
     Sets the value of the environment variable at the Machine-level environment variable scope.
-    If running on Windows and the current session is not elevated, causes an exception to be thrown.
+    Not supported on non-Windows platforms.
 .PARAMETER Scope
     The scope of the environment variable to set.
-    If running on Windows and the current session is not elevated, values other than 'Process' cause an exception to be thrown.
+    Values other than 'Process' (the default) are not supported on non-Windows platforms.
 .PARAMETER Name
     The name of the environment variable to set.
 .PARAMETER Value
     The value to set for the environment variable.
 .PARAMETER KVP
-    A key-value pair whose key is the name of the environment variable to set and whose value is the value to set for the environment variable.
+    A key-value pair whose key and value are the environment variable to set.
 .PARAMETER Entry
-    A dictionary entry whose key is the name of the environment variable to set and whose value is the value to set for the environment variable.
+    A dictionary entry whose key and value are the environment variable to set.
 .PARAMETER Environment
-    A hashtable whose keys are the names of the environment variables to set and whose values are the values to set for the environment variables.
-.PARAMETER SkipOverwrite
-    If specified, environment variables that already exist will not be overwritten.
+    A hashtable whose keys and values are the the environment variables to set.
 .EXAMPLE
     Set-EnvVar -Process -Name "MYAPP_HOME" -Value "C:\Program Files\MyApp"
 .EXAMPLE
-    Set-EnvVar -Process -SkipOverwrite -Environment @{MYAPP_HOME="C:\Program Files\MyApp"; MYAPP_DATA="C:\ProgramData\MyApp"}
+    Set-EnvVar -Process -Environment @{MYAPP_HOME="C:\Program Files\MyApp"; MYAPP_DATA="C:\ProgramData\MyApp"}
 .COMPONENT
     env
 #>
 function Set-EnvVar() {
-    [CmdletBinding(DefaultParameterSetName = "ProcessScopeNAV")]
+    [CmdletBinding(DefaultParameterSetName = "ProcessScopeVAName")]
     param(
-        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeNAV", Position=0)]
+        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeVAName", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="MachineScopeKVP", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="MachineScopeDE", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="MachineScopeHashtable", Position=0)]
+        [ValidateScript({ $IsWindows }, ErrorMessage="The Machine scope is not supported on non-Windows platforms.")]
         [switch] $Machine,
 
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeNAV", Position=0)]
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeKVP", Position=0)]
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeDE", Position=0)]
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeHashtable", Position=0)]
+        [Parameter(Mandatory=$false, ParameterSetName="ProcessScopeVAName", Position=0)]
+        [Parameter(Mandatory=$false, ParameterSetName="ProcessScopeKVP", Position=0)]
+        [Parameter(Mandatory=$false, ParameterSetName="ProcessScopeDE", Position=0)]
+        [Parameter(Mandatory=$false, ParameterSetName="ProcessScopeHashtable", Position=0)]
         [switch] $Process,
 
-        [Parameter(Mandatory=$true, ParameterSetName="UserScopeNAV", Position=0)]
+        [Parameter(Mandatory=$true, ParameterSetName="UserScopeVAName", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="UserScopeKVP", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="UserScopeDE", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="UserScopeHashtable", Position=0)]
+        [ValidateScript({ $IsWindows }, ErrorMessage="The User scope is not supported on non-Windows platforms.")]
         [switch] $User,
 
-        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueNAV", Position=0)]
+        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueVAName", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="ScopeValueKVP", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="ScopeValueDE", Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName="ScopeValueHashtable", Position=0)]
+        [ValidateScript({ ($_ -eq [System.EnvironmentVariableTarget]::Process) -or $IsWindows }, ErrorMessage="Only the Process scope is supported on non-Windows platforms.")]
         [System.EnvironmentVariableTarget] $Scope,
 
-        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeNAV", Position=1, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeNAV", Position=1, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="UserScopeNAV", Position=1, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueNAV", Position=1, ValueFromPipelineByPropertyName=$true)]
-        [ValidateLength(1, [int]::MaxValue)]
+        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeVAName", Position=1, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeVAName", Position=1, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="UserScopeVAName", Position=1, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueVAName", Position=1, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
         [string] $Name,
 
-        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeNAV", Position=2, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeNAV", Position=2, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="UserScopeNAV", Position=2, ValueFromPipelineByPropertyName=$true)]
-        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueNAV", Position=2, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="MachineScopeVAName", Position=2, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="ProcessScopeVAName", Position=2, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="UserScopeVAName", Position=2, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName="ScopeValueVAName", Position=2, ValueFromPipelineByPropertyName=$true)]
         [object] $Value,
 
         [Parameter(Mandatory=$true, ParameterSetName="MachineScopeKVP", Position=1, ValueFromPipeline=$true)]
@@ -99,14 +100,16 @@ function Set-EnvVar() {
         [switch] $SkipOverwrite
     )
     Begin {
-        if ($Machine -and $Machine.IsPresent) {
+        if ($Machine) {
             $Scope = [System.EnvironmentVariableTarget]::Machine
-        } elseif ($Process -and $Process.IsPresent) {
-            $Scope = [System.EnvironmentVariableTarget]::Process
-        } elseif ($User -and $User.IsPresent) {
+        } elseif ($User) {
             $Scope = [System.EnvironmentVariableTarget]::User
+        } elseif ($Process) {
+            $Scope = [System.EnvironmentVariableTarget]::Process
         }
-
+        if (-not (Get-Variable -Name "Scope" -ErrorAction SilentlyContinue)) {
+            $Scope = [System.EnvironmentVariableTarget]::Process
+        }
         if (-not [System.EnvironmentVariableTarget]::IsDefined($Scope)) {
             throw "Unrecognized EnvironmentVariableTarget '$Scope'"
         }
